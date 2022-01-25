6fae784bacec4a2d55d064d66deb654107364f93
harsimran-preet <59977147+harsimran-preet@users.noreply.github.com>
2022-01-25 01:00:05 -0800

    complete

--- stock-portfolio.js
+++ stock-portfolio.js
@@ -12,20 +12,77 @@ function num_of_tickers(portfolio){
     res = [... new Set(portfolio)]
     return res.length;
 }
-// function buyingshares(portfolio, ticker, num_of_shares){
-//     if (ticker in portfolio){
-//         portfolio[ticker] += num_of_shares;
+
+
+// function buyingshares(port, ticker, num_of_shares){
+//     for (let x = 0; x<port.length; x++){
+//         if (port[i][0] )
 //     }
-//     else{
-//         portfolio[ticker] = 0;
-//         portfolio[ticker] = num_of_shares;
-//     }
-//     return portfolio;
-// }
+    // if (port[ticker]){
+    //     port[ticker] +=num_of_shares;
+    // }
+    // else{
+    //     port[ticker] = 0;
+    //     port[ticker] = num_of_shares;
+    // }
+    // return port;
+//}
 // 4 is pending
 
+function sellshares(port, ticker, num_of_shares){
+    port = take_out_empty(port);
+    for (let x = 0; x<port.length; x++){
+        if (port[x][0] === ticker){
+            if (port[x][1]<num_of_shares){
+                throw "ShareSaleException";
+            }
+            else{
+                port[x][1] -= num_of_shares;
+            }
+        }
+    }
+    port = take_out_empty(port);
+    return port;
+}
+function take_out_empty(portfolio){
+    for (let x = 0; x<portfolio.length; x++){
+        if (portfolio[x][1] < 1){
+            portfolio.splice(x, 1);
+        }
+    }
+    return portfolio;
+}
 
-exports.buyingshares = buyingshares;
+function buyshares(portfolio, ticker, num_of_shares){
+    portfolio = take_out_empty(portfolio);
+    tickers = []
+    for (let x = 0; x<portfolio.length; x++){
+        if(!(tickers.includes(portfolio[x][0]))){
+            tickers.push(portfolio[x][0]);
+        }
+        if (portfolio[x][0] == ticker){
+            portfolio[x][1] += num_of_shares;
+        }
+    }
+    if(!(tickers.includes(ticker))){
+        portfolio.push([ticker, num_of_shares]);
+    }
+    return portfolio;
+}
+
+function search_share(portfolio, ticker){
+    portfolio = take_out_empty(portfolio);
+    for (let x = 0; x<portfolio.length; x++){
+        if (portfolio[x][0] == ticker){
+            return portfolio[x][1];
+        }
+    }
+    return;
+}
+exports.search_share = search_share;
+exports.take_out_empty = take_out_empty;
+exports.sellshares = sellshares;
+exports.buyshares = buyshares;
 exports.num_of_tickers = num_of_tickers;
 exports.ifempty = ifempty;
 exports.settingup = settingup;
--- stock-portfolio.test.js
+++ stock-portfolio.test.js
@@ -17,5 +17,52 @@ test('Test number of unique tickets -- multiple tickers', () => {
   });
 
   // test('Test buying shares for a symbol -- not already have', () => {
-  //   expect(myFunctions.num_of_tickers({"": 0}, "GMR", 3)).toBe({"GMR": 3});
+  //   var portfolio = {"GMR": 10}
+  //   var res = {"GMR": 10, "TSLA": 3}
+  //   expect(myFunctions.num_of_tickers(portfolio, "TSLA", 3)).toBe(res);
   // });
+  test('Test to buy share -- for an existing ticker', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.buyshares(portfolio, "GMR", 10)).toStrictEqual([["ABC", 30],["GMR", 20]]);
+  });
+  test('Test to buy share -- for non-existing ticker', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.buyshares(portfolio, "TSLA", 10)).toStrictEqual([["ABC", 30],["GMR", 10], ["TSLA", 10]]);
+  });
+test('Test selling shares for a symbol -- normal', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.sellshares(portfolio, "GMR", 5)).toStrictEqual([["ABC", 30], ["GMR", 5]]);
+  });
+
+test('Test selling shares for a symbol -- ticker that is not owned', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.sellshares(portfolio, "TSLA", 5)).toStrictEqual([["ABC", 30], ["GMR", 10]]);
+  });
+
+test('Test selling shares for a symbol -- selling all of a ticker', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.sellshares(portfolio, "ABC", 30)).toStrictEqual([["GMR", 10]]);
+  });
+test('Test selling shares for a symbol -- sell more than have', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect (() => {myFunctions.sellshares(portfolio, "GMR", 15)}).toThrow("ShareSaleException");
+
+  });
+
+  test('Test seaching for a ticker -- normal', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.search_share(portfolio, "ABC")).toBe(30);
+  });
+  test('Test seaching for a ticker -- for the one that is not owned', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.search_share(portfolio, "TSLA")).toBe();
+  });
+
+  test('Test to find only owned ticker list -- no empty', () => {
+    portfolio = [["ABC", 30], ["GMR", 10]];
+    expect(myFunctions.take_out_empty(portfolio)).toStrictEqual([["ABC", 30],["GMR", 10]]);
+  });
+  test('Test to find only owned ticker list -- with an empty ticker', () => {
+    portfolio = [["ABC", 30], ["GMR", 10], ["TSLA", 0]];
+    expect(myFunctions.take_out_empty(portfolio)).toStrictEqual([["ABC", 30],["GMR", 10]]);
+  });
